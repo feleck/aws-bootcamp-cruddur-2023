f06da15a12635e5b4f202b8aebd28f0417712302
Marcin <feleck@users.noreply.github.com>
2023-03-24 11:19:22 +0000

    added real cognito user to db seed

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,12 +1,13 @@
 INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrewbrown', 'ab@email.com' ,'MOCK'),
+  ('Mike Notandrew', 'araaargggggh', 'ab@email.com' ,'MOCK'),
   ('Andrew Bayko', 'bayko', 'ba@email.com', 'MOCK'),
-  ('Londo Mollari', 'londo', 'lmollari@centari.com', 'MOCK');
+  ('Londo Mollari', 'londo', 'lmollari@centari.com', 'MOCK'),
+  ('Another User', 'auser', 'testin@email.com', 'MOCK');
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'araaargggggh' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
