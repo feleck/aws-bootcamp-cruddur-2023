4a6f99bd323974d471b4623bebdc8a647fab085c
Marcin Felczak <feleck@poczta.onet.pl>
2023-04-07 13:40:44 +0200

    fixed exirng token

--- frontend-react-js/src/pages/MessageGroupNewPage.js
+++ frontend-react-js/src/pages/MessageGroupNewPage.js
@@ -6,7 +6,7 @@ import DesktopNavigation  from '../components/DesktopNavigation';
 import MessageGroupFeed from '../components/MessageGroupFeed';
 import MessagesFeed from '../components/MessageFeed';
 import MessagesForm from '../components/MessageForm';
-import CheckAuth from '../lib/CheckAuth';
+import {CheckAuth} from '../lib/CheckAuth';
 
 export default function MessageGroupPage() {
   const [otherUser, setOtherUser] = React.useState([]);
