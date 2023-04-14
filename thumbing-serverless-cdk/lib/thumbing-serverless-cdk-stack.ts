9e73b9cf1c1960310a3cb84abc3937ad73747b63
Marcin Felczak <feleck@poczta.onet.pl>
2023-04-14 16:40:13 +0200

    added serverless avatar processing-partial

---
+++ thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
@@ -0,0 +1,68 @@
+import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3'
+import * as lambda from 'aws-cdk-lib/aws-lambda'
+import { Construct } from 'constructs';
+import * as dotenv from 'dotenv';
+import * as s3n from 'aws-cdk-lib/aws-s3-notifications'
+
+dotenv.config();
+
+export class ThumbingServerlessCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+    const bucketName: string = process.env.THUMBING_BUCKET_NAME as string;
+    const functionPath: string = process.env.THUMBING_FUNCTION_PATH as string;
+    const folderInput: string = process.env.THUMBING_S3_FOLDER_INPUT as string;
+    const folderOutput: string = process.env.THUMBING_S3_FOLDER_OUTPUT as string;
+    const webhookUrl: string = process.env.THUMBING_WEBHOOK_URL as string;
+    const topicName: string = process.env.THUMBING_TOPIC_NAME as string;
+
+    // const bucket = this.createBucket(bucketName);
+    const bucket = this.importBucket(bucketName);
+    // need to create S3 bucket by hand and use it (import here) "assets.awsbootcamp.online"
+    // create folders avatars and inside that: "oryginal" and "processed" in bucket (by hand)
+    const lambda = this.createLambda(functionPath, bucketName, folderInput, folderOutput);
+
+    this.createS3NotifyToLambda(folderInput, lambda, bucket)
+  }
+
+  createBucket(bucketName: string): s3.IBucket {
+    const bucket = new s3.Bucket(this, 'ThumbingBucket', {
+      bucketName: bucketName,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
+    return bucket;
+  }
+
+  importBucket(bucketName: string): s3.IBucket {
+    const bucket = s3.Bucket.fromBucketName(this, "AssetsBucket", bucketName);
+    return bucket;
+  }
+
+  createLambda(functionPath: string, bucketName: string, folderInput: string, folderOutput: string ) : lambda.IFunction {
+    const lambdaFunction = new lambda.Function(this, 'ThumbLambda', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(functionPath),
+      environment: {
+        DEST_BUCKET_NAME: bucketName,
+        FOLDER_INPUT: folderInput,
+        FOLDER_OUTPUT: folderOutput,
+        PROCESS_WIDTH: '512',
+        PROCESS_HEIGHT: '512'
+      }
+    });
+    return lambdaFunction;
+  }
+
+  createS3NotifyToLambda(prefix: string, lambda: lambda.IFunction, bucket: s3.IBucket): void {
+    const destination = new s3n.LambdaDestination(lambda);
+    bucket.addEventNotification(s3.EventType.OBJECT_CREATED_POST,
+      destination,
+      {
+        prefix: prefix //folder containig original images
+      })
+  }
+}
