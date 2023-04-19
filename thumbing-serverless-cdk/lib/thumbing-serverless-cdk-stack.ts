e99450032ddac473e2f79ce30f6521e9c0f26bea
Marcin Felczak <feleck@poczta.onet.pl>
2023-04-19 15:27:32 +0200

    updated example lambda and added iam permissions to cdk

--- thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
+++ thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
@@ -2,8 +2,9 @@ import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3'
 import * as lambda from 'aws-cdk-lib/aws-lambda'
 import { Construct } from 'constructs';
-import * as dotenv from 'dotenv';
 import * as s3n from 'aws-cdk-lib/aws-s3-notifications'
+import * as iam from 'aws-cdk-lib/aws-iam'
+import * as dotenv from 'dotenv';
 
 dotenv.config();
 
@@ -25,7 +26,9 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
     // create folders avatars and inside that: "oryginal" and "processed" in bucket (by hand)
     const lambda = this.createLambda(functionPath, bucketName, folderInput, folderOutput);
 
-    this.createS3NotifyToLambda(folderInput, lambda, bucket)
+    this.createS3NotifyToLambda(folderInput, lambda, bucket);
+    const s3ReadWritePolicy = this.createPolicyBucketAccess(bucket.bucketArn);
+    lambda.addToRolePolicy(s3ReadWritePolicy);
   }
 
   createBucket(bucketName: string): s3.IBucket {
@@ -66,5 +69,18 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
         prefix: prefix //folder containig original images
       }
     )
+  }
+
+  createPolicyBucketAccess(bucketArn: string) {
+    const s3ReadWritePolicy = new iam.PolicyStatement({
+      actions: [
+        's3:GetObject',
+        's3:PutObject',
+      ],
+      resources: [
+        `${bucketArn}/*`,
+      ]
+    });
+    return s3ReadWritePolicy;
   }
 }
