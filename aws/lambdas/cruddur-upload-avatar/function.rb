744615418a20a570c7c8e6a90c5ae8485a37e74d
Marcin <feleck@gmail.com>
2023-05-26 09:57:00 +0000

    ruby upload function corrections

--- aws/lambdas/cruddur-upload-avatar/function.rb
+++ aws/lambdas/cruddur-upload-avatar/function.rb
@@ -1,28 +1,47 @@
 require 'aws-sdk-s3'
 require 'json'
+require 'jwt'
 
 def handler(event:, context:)
   puts event
-  s3 = Aws::S3::Resource.new
-  
-  bucket_name = ENV['UPLOADS_BUCKET_NAME']
-  
-  object_key = 'mock.jpg'
-  
-  obj = s3.bucket(bucket_name).object(object_key)
-  url = obj.presigned_url(:put, expires_in: 60 * 5)
-  body = { url: url}.to_json
-  { 
+  # CORS headers - preflight check
+  if event['routeKey'] == "OPTIONS /{proxy+}"
+    puts ({step: 'preflight', message: 'preflight CORS check'}.to_json)
+    {
+      headers: {
+        "Access-Control-Allow-Headers": "*, Authorization",
+        "Access-Control-Allow-Origin": "https://3000-feleck-awsbootcampcrudd-5cokladl5r9.ws-eu98.gitpod.io",
+        "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
+      },
+      statusCode: 200
+    }
+  else
+    token = event['headers']['authorization'].split(' ')[1]
+    puts ({step: 'presignedurl', access_token: token}.to_json)
+    body_hash = JSON.parse(e["body"])
+    extension = body_hash["extension"]
+    decoded_token = JWT.decode token, nil, false
+    puts 'decoded token'
+    cognito_user_uuid = decoded_token['sub']
+
+    s3 = Aws::S3::Resource.new
+    bucket_name = ENV['UPLOADS_BUCKET_NAME']
+    object_key = "#{cognito_user_uuid}.#{extension}"
+    
+    puts ({object_key: object_key}.to_json)
+    
+    obj = s3.bucket(bucket_name).object(object_key)
+    url = obj.presigned_url(:put, expires_in: 60 * 5)
+    url # data that will be returned
+    body = { url: url}.to_json
+    {
     headers: {
       "Access-Control-Allow-Headers": "*, Authorization",
       "Access-Control-Allow-Origin": "https://3000-feleck-awsbootcampcrudd-j5mzmw6f17m.ws-eu96b.gitpod.io",
       "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
     },
-    statusCode: 200, body: body 
-  }
+    statusCode: 200, 
+    body: body 
+    }
+  end
 end
-
-puts handler(
-  event:{},
-  context: {}
-)
