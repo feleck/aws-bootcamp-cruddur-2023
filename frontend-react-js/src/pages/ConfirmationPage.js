477eb8b6d30dfc2487a35c9fbeac66ef92b1c3d4
Marcin <feleck@gmail.com>
2023-07-24 14:36:28 +0000

    frontend fixes clean up and build

--- frontend-react-js/src/pages/ConfirmationPage.js
+++ frontend-react-js/src/pages/ConfirmationPage.js
@@ -1,7 +1,7 @@
 import './ConfirmationPage.css';
 import React from "react";
 import { useParams } from 'react-router-dom';
-import {ReactComponent as Logo} from '../components/svg/logo.svg';
+import {ReactComponent as Logo} from 'components/svg/logo.svg';
 
 // [TODO] Authenication
 import { Auth } from 'aws-amplify';
